1302733e0a8cda6a93745bb928e78abe64c2bc5f
P208252 <hrpp1353@gmail.com>
2026-01-12 15:57:32 +0900

    fix: Supabase 환경 변수 이름 수정

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -2,8 +2,8 @@
 
 interface ImportMetaEnv {
   readonly VITE_DEV: string;
-  readonly VITE_SUPABASE_URL: string;
-  readonly VITE_SUPABASE_ANON_KEY: string;
+  readonly SUPABASE_URL: string;
+  readonly SUPABASE_ANON_KEY: string;
   // 다른 환경 변수들을 여기에 추가하세요
 }
 
